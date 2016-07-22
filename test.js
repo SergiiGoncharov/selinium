d127f42e874d9f484b8776046ce347d7c7ec30f3
Sergii Goncharov <etype@bk.ru>
2016-07-23 00:41:10 +0300

    now work with Edge

--- test.js
+++ test.js
@@ -5,10 +5,22 @@ var webdriver = require('selenium-webdriver'),
     By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until,
     fs = require('fs');
-
+/*
 var driver = new webdriver.Builder()
-    .forBrowser('chrome')
+    .forBrowser('edge')
     .build();
+
+*/
+var edge = require('selenium-webdriver/edge');
+var service = new edge.ServiceBuilder()
+ .usingPort(55555)
+ .build();
+var options = new edge.Options();
+// configure browser options ...
+
+var driver = new edge.Driver(options, service);
+
+
 
 var post_link_selector = '.wpb_wrapper .item-details .entry-title a';
 
@@ -22,8 +34,11 @@ driver.manage().window().setSize(1200, 2000); //not work as expected
 driver.get('http://www.pausefun.com/');
 driver.wait(until.elementsLocated(By.css(post_link_selector)));
 driver.findElement(By.css(post_link_selector)).click().then(e => {
-	driver.sleep(5000);
+	driver.sleep(3000);
 });
+
+let zoom = 0.2;
+driver.executeScript(`document.body.style.transform='scale(${zoom}) translate(0, -${(1-zoom)/2/zoom*100}%)'`); //(1-0.9)/2/0.9
 
 driver.wait(() => {
 		return driver.executeScript('return document.readyState').then(state => {
@@ -31,35 +46,35 @@ driver.wait(() => {
 			return state === 'complete';
 		});
 	}).then( () => {
-		driver.sleep(10*1000)
+		driver.sleep(3*1000);
+		//return;
 	}).then( () => {
 		console.log('times up');
+		//return;
 	});
 
 
-driver.executeScript('return document.title').then(function(d){console.log(d);});
-driver.executeScript('return document.readyState').then(function(d){console.log(d);});
+let elemmm = driver.findElement(By.css('#div-gpt-ad-1466004631028-2'));
 
-
-
-//driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-//driver.wait();
-
-
-driver.takeScreenshot().then(function(data){
+elemmm.takeScreenshot(true).then(function(data){
    var base64Data = data.replace(/^data:image\/png;base64,/,"")
-   fs.writeFile("out2.png", base64Data, 'base64', function(err) {
+   fs.writeFile("elem.png", base64Data, 'base64', function(err) {
         if(err) console.log(err);
    });
 });
 
 
-/*
-driver.findElement(By.css('#div-gpt-ad-1466004631028-0')).takeScreenshot(true).then(data => {
+
+
+driver.takeScreenshot().then(data => {
 	var base64Data = data.replace(/^data:image\/png;base64,/,"")
-   fs.writeFile("elem.png", base64Data, 'base64', function(err) {
+   fs.writeFile("screen.png", base64Data, 'base64', function(err) {
         if(err) console.log(err);
    });
-})
-*/
+});
+
+
+driver.executeScript('return document.title').then(function(d){console.log(d);});
+driver.executeScript('return document.readyState').then(function(d){console.log(d);});
+driver.sleep(1000);
 driver.quit();
